c5380d52c1733deefde2f23574d15ea477e29087
Aleksandr Zhivov <aulifritamin@gmail.com>
2025-10-23 22:58:41 -0700

    code rafactored

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -3,11 +3,9 @@ using UnityEngine;
 public class SpawnPoint : MonoBehaviour
 {
     [SerializeField] private Target _assignedTarget;
-    [SerializeField] private Enemy _enemyPrefab;
     [SerializeField] private Color _enemyColor;
     
     public Target AssignedTarget => _assignedTarget;
-    public Enemy EnemyPrefab => _enemyPrefab;
     public Transform Transform => transform;
     public Color EnemyColor => _enemyColor;
 }
